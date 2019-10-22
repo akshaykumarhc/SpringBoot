3fded2181a87987edde2539b2225cb54acb71134
Akshay Kumar <akshaykumar@LK-LP-756.local>
2019-10-22 18:27:26 +0530

    Committing some other APIs

--- src/main/java/com/springboot/starting/controller/ApplicationRestController.java
+++ src/main/java/com/springboot/starting/controller/ApplicationRestController.java
@@ -3,9 +3,7 @@ package com.springboot.starting.controller;
 import com.springboot.starting.model.Topic;
 import com.springboot.starting.model.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,20 @@ public class ApplicationRestController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public Topic addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+        return topicService.getTopic(topic.getId());
+    }
+    @RequestMapping(method = RequestMethod.PUT,value = "/topics/{id}")
+    public Topic updateTopic(@PathVariable String id , @RequestBody Topic topic){
+        topicService.updateTopic(id,topic);
+        return topicService.getTopic(topic.getId());
+    }
+    @RequestMapping(method = RequestMethod.DELETE,value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
     }
 }
--- src/main/java/com/springboot/starting/model/TopicService.java
+++ src/main/java/com/springboot/starting/model/TopicService.java
@@ -2,24 +2,40 @@ package com.springboot.starting.model;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("001", "Java", "This course will give you basic !dea of Java."),
             new Topic("002", "Spring", "This course will give you basic !dea of Spring."),
             new Topic("003", "SQL", "This course will give you basic !dea of SQL."),
             new Topic("004", "Micro service", "This course will give you basic !dea of Micro service."),
             new Topic("005", "Mongo DB", "This course will give you basic !dea of Mongo DB.")
-    );
+    ));
 
     public List<Topic> getTopics() {
         return topics;
     }
 
-    public Topic getTopic(String id) {
+    public
+    Topic getTopic(String id) {
         return topics.stream().filter(i->i.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        Topic temp = topics.stream().filter(i->i.getId().equals(id)).findFirst().get();
+        temp.setDescription(topic.getDescription());
+        temp.setName(topic.getName());
+    }
+
+    public void deleteTopic(String id) {
+        topics.remove(topics.stream().filter(i->i.getId().equals(id)).findFirst().get());
     }
 }
