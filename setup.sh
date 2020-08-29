cf24882f80bdb8de9b6bae7820c0fb7322bc5bf5
akshaykumarhc <meetakshaykumar@gmail.com>
2020-08-29 11:28:29 +0000

    kafka last try

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 
 /home/gitpod/confluent-5.5.0/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
-Running in a "--no-prompt" mode
+/home/gitpod/confluent-5.5.0/bin/confluent local start
 
-confluent local start
+mvn clean install -P jar -DskipTests
