996f62d011d4497c1e612eadea35c1a1bdbf48fd
akshaykumarhc <meetakshaykumar@gmail.com>
2020-08-29 15:34:06 +0000

    last commit

--- setup.sh
+++
@@ -1,5 +0,0 @@
-
-/home/gitpod/confluent-5.5.0/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
-/home/gitpod/confluent-5.5.0/bin/confluent local start
-
-mvn clean install -P jar -DskipTests
